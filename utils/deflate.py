3b727247513cdd9b96bd08a641864b8b1cd600db
eV <ev@7pr.xyz>
2024-08-13 03:33:47 +0000

    python3 compat for utils/deflate

--- utils/deflate.py
+++ utils/deflate.py
@@ -2,17 +2,21 @@
 import argparse
 import zlib
 
+
 def compress(in_fn, out_fn):
-    compressor = zlib.compressobj(9, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
-    with open(in_fn) as in_f:
+    compressor = zlib.compressobj(
+        9, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0
+    )
+    with open(in_fn, "rb") as in_f:
         dat = in_f.read()
         deflated = compressor.compress(dat)
         deflated += compressor.flush()
-        if out_fn == '-':
-            print deflated
+        if out_fn == "-":
+            print(deflated)
         else:
-            with open(out_fn, 'w+') as out_f:
+            with open(out_fn, "wb+") as out_f:
                 out_f.write(deflated)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -21,5 +25,6 @@ def main():
     args = parser.parse_args()
     compress(args.input, args.output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
